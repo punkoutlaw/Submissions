0310492476646deef3182cb2f1739aa41b6496eb
David Feudale <dwfeudale@gmail.com>
2022-05-13 10:51:29 -0400

    fixed div properties

--- W14-Movie-App/src/components/movie.js
+++ W14-Movie-App/src/components/movie.js
@@ -15,7 +15,7 @@ class Main extends React.Component {
                             <br></br>
                             <Card.Title>Harry Potter and the Goblet of Fire</Card.Title>
                             </div>
-                            <div className='card-img'>
+                            <div className='card-body'>
                             <br></br>
                             <Card.Img variant="top" className="center" src="https://static.free-putlockers.com/dist/images/McpiUC-e_JqUZKWBZZT0GJ7mUQ7KsPfDtKRKQ-tiCPmIuPm9m6bsHaMBPxo-WCAWgbydIVeNpoV9t-w_aME5RLOjJeb6LLXKjbZX3Xiotdyb1nDGfoNftJLgmTnV7_9u.jpg" />
                             </div>
@@ -43,7 +43,7 @@ class Main extends React.Component {
                     <br></br>
                     <Card.Title>IT</Card.Title>
                     </div>
-                    <div className='card-img'>
+                    <div className='card-body'>
                     <br></br>
                     <Card.Img variant="top" className="center" src="https://th.bing.com/th/id/R.ea6fb4e0a4674574a344134063d68475?rik=p%2b73sLjtfftJ4w&riu=http%3a%2f%2forig00.deviantart.net%2f4737%2ff%2f2016%2f202%2fb%2f2%2fstephen_king_s_it__2017____poster___1_by_camw1n-daa4tl6.jpg&ehk=cMXs7fWyY0zEpXJMlfmG4bgHefwVOSDDSyeBp9XjA8M%3d&risl=&pid=ImgRaw&r=0" />
                     </div>
@@ -71,7 +71,7 @@ class Main extends React.Component {
                             <br></br>
                         <Card.Title>Almost Famous</Card.Title>
                         </div>
-                    <div className='card-img'>
+                    <div className='card-body'>
                         <br></br>
                     <Card.Img variant="top" className="center" src="https://m.media-amazon.com/images/M/MV5BMzY1ZjMwMGEtYTY1ZS00ZDllLTk0ZmUtYzA3ZTA4NmYwNGNkXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_FMjpg_UX1000_.jpg" />
                     </div>
