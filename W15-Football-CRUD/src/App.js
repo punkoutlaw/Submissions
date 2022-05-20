92460474f343703199452258d1ced9c8e179fc20
David Feudale <dwfeudale@gmail.com>
2022-05-19 23:15:04 -0400

    Added W15 Project files

---
+++ W15-Football-CRUD/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Main from './components/Main';
+import AddUser from './components/AddUser';
+import EditUser from './components/EditUser';
+import { GlobalProvider } from './context/GlobalState';
+
+function App() {
+  return (
+
+    <div className="App">
+        <header className="bg-dark">
+          <h1>Fantasy Football</h1>  
+        </header>
+        <div className='card-body'>
+        <GlobalProvider>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Main />} />
+              <Route path="/Add" element={<AddUser />} />
+              <Route path="/Edit/:id" element={<EditUser />} />
+            </Routes>
+          </Router>
+        </GlobalProvider>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ W15-Football-CRUD/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import UserList from './UserList'
+
+const Main = () => {
+  return (
+    <div>
+        <Header />
+        <UserList />
+    </div>
+  )
+}
+
+export default Main
