312f8113543cf178c18ac1243aa72b0f55636baa
David Feudale <dwfeudale@gmail.com>
2022-05-25 19:04:40 -0400

    fixed edit user issue

--- W15-Football-CRUD/src/components/EditUser.js
+++ W15-Football-CRUD/src/components/EditUser.js
@@ -3,45 +3,51 @@ import { GlobalContext } from '../context/GlobalState'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
-const EditUser = (props) => {
-const [selectedUser, setSelectedUser] = useState({
-  id: '',
-  name: ''
-});
-const { users, editUser } = useContext(GlobalContext);
-const navigate = useNavigate();
-const currentUserId = useParams(props);
+const EditUser = () => {
+  const navigate = useNavigate();
+  const { userId } = useParams();
+  const { users, editUser } = useContext(GlobalContext);
 
-useEffect(() => {
-  const userId = currentUserId;
-  const selectedUser = users.find((user) => user.id === userId);
-  if (selectedUser) {
-    setSelectedUser(selectedUser);
-  } 
-}, [currentUserId, users]);
+  const [selectedUser, setSelectedUser] = useState(
+    users.find((user) => user.id === userId)
+  );
 
-const onSubmit = (e) => {
-  e.preventDefault()
-  editUser(selectedUser)
-  navigate('/');
-}
+  useEffect(() => {
+    if (!selectedUser) navigate(-1);
+  }, [navigate, selectedUser]);
 
-const onChange = (e) => {
-  setSelectedUser({...selectedUser, [e.target.name]: e.target.value})
-}
+  const onSubmit = (e) => {
+    e.preventDefault();
+    editUser(selectedUser);
+    navigate("/");
+  };
+
+  const onChange = (e) => {
+    setSelectedUser({ ...selectedUser, [e.target.name]: e.target.value });
+  };
 
   return (
-    <div className='container w-25'>
-        <Form className='form-control' onSubmit={onSubmit}>
-            <FormGroup>
-                <Label>Edit Name</Label>
-                <Input type='text' name='name' value={selectedUser.name} onChange={onChange} placeholder='Enter Name'></Input>
-            </FormGroup>
-        <Button type='submit'>Edit Name</Button>
-        <Link to='/' className='btn btn-danger m-2'>Back</Link>
+    <div className="container w-25">
+      {selectedUser && (
+        <Form className="form-control" onSubmit={onSubmit}>
+          <FormGroup>
+            <Label>Edit Name</Label>
+            <Input
+              type="text"
+              name="name"
+              value={selectedUser.name}
+              onChange={onChange}
+              placeholder="Enter Name"
+            />
+          </FormGroup>
+          <Button type="submit">Edit Name</Button>
+          <Link to="/" className="btn btn-danger m-2">
+            Back
+          </Link>
         </Form>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default EditUser
